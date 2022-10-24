050bb7783c0b0e95d3d5cf3e6be47ad5e7fee418
[carlos martinez] <theycallhellp@gmail.com>
2022-10-23 22:02:55 -0700

    test employee object

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,45 @@
+const Employee = require('../src/Employee');
+
+describe('Employee', () => {
+    describe('Initialization', () => {
+        it('Should create an object with a name, id, and email attached', () => {
+            const employee = new Employee('Jake', '3242', 'fake@email.com')
+    
+            expect(employee.name).toEqual('Jake');
+            expect(employee.id).toEqual('3242');
+            expect(employee.email).toEqual('fake@email.com');
+        });
+    });
+
+    describe('get name', () => {
+        it('Should return employee name', () => {
+            const employee = new Employee('Jake', '3242', 'fake@email.com')
+    
+            expect(employee.getName()).toEqual('Jake');
+        });
+    });
+
+    describe('get id', () => {
+        it('Should return employee id', () => {
+            const employee = new Employee('Jake', '3242', 'fake@email.com')
+    
+            expect(employee.getId()).toEqual('3242');
+        });
+    });
+
+    describe('get name', () => {
+        it('Should return employee email', () => {
+            const employee = new Employee('Jake', '3242', 'fake@email.com')
+    
+            expect(employee.getEmail()).toEqual('fake@email.com');
+        });
+    });
+
+    describe('get name', () => {
+        it('Should return employee role', () => {
+            const employee = new Employee('Jake', '3242', 'fake@email.com')
+    
+            expect(employee.getRole()).toEqual('Employee');
+        });
+    });
+})
