b7b4c9fc94e88b1075bb951f99e8aa48057531c3
[carlos martinez] <theycallhellp@gmail.com>
2022-10-24 23:45:14 -0700

    create team page

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const managerQuestions = [
     {message: 'Enter manager name!', name: 'name'},
     {message: 'Enter manager employee ID!', name: 'id'},
     {message: 'Enter manager email address!', name: 'email'},
-    {message: 'Enter manager office number!', name: 'office'}
+    {message: 'Enter manager office number!', name: 'officeNumber'}
 ];
 
 const engineerQuestions = [
@@ -55,44 +55,105 @@ const repeatQuestion = [
 ]
 
 function writeTeamPage(team) {
-    let fileText = 'ENTER HTML CARD'; //CREATES HTML DOCUMENT -> use team cards
+    let fileText =
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/0f88f0a4a7.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <title>Team Project</title>
+</head>
+<body>
+    <h1 class="text-center bg-danger text-white py-5 mb-5">My Team</h1>
+    <div class="container">
+        <div class="row mx-3 gx-3">
+
+            <div class="col-md-6 col-lg-4 px-3">
+                <div class="card mb-3 shadow">
+                    <div class="card-header bg-primary text-white">
+                    <h2>Name</h2>
+                    <h3><i class="fa-solid fa-mug-hot"></i> Manager</h3>
+                    </div>
+                    <div class="card-body bg-light py-3">
+                        <p class="bg-body border p-2 mb-0">ID: enter ID</p>
+                        <p class="bg-body border border-top-0 border-bottom-0 p-2 mb-0">Email: <a href="mailto:fake@email.com">fake@email.com</a></p>
+                        <p class="bg-body border p-2 my-0">Office number: 1</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 col-lg-4 px-3">
+                <div class="card mb-3 shadow">
+                    <div class="card-header bg-primary text-white">
+                    <h2>Name</h2>
+                    <h3><i class="fa-solid fa-glasses"></i> Engineer</h3>
+                    </div>
+                    <div class="card-body bg-light py-3">
+                        <p class="bg-body border p-2 mb-0">ID: enter ID</p>
+                        <p class="bg-body border border-top-0 border-bottom-0 p-2 mb-0">Email: <a href="mailto:fake@email.com">fake@email.com</a></p>
+                        <p class="bg-body border p-2 my-0">Github: <a href="https://github.com/carmart7">carmart7</a></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 col-lg-4 px-3">
+                <div class="card mb-3 shadow">
+                    <div class="card-header bg-primary text-white">
+                    <h2>Name</h2>
+                    <h3><i class="fa-solid fa-user-graduate"></i> Intern</h3>
+                    </div>
+                    <div class="card-body bg-light py-3">
+                        <p class="bg-body border p-2 mb-0">ID: enter ID</p>
+                        <p class="bg-body border border-top-0 border-bottom-0 p-2 mb-0">Email: <a href="mailto:fake@email.com">fake@email.com</a></p>
+                        <p class="bg-body border p-2 my-0">School: 1</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>`; //CREATES HTML DOCUMENT -> use team cards
     fs.writeFile(`teamPage.html`, fileText, (err) => {
         if (err) {
             console.error(err);
         } else {
             console.log('Writing team html page was a success!');
         }
-    })
+    });
 }
 
-function createManagerCard (data) {
-    // create card with data
-
-    // return card
+function createManager (data) {
+    // create Manager class object
+    return new Manager(data.name, data.id, data.email, data.officeNumber);
+    // return object created
 }
 
-function createEngineerCard (data) {
-    // create card with data
-
-    // return card
+function createEngineer (data) {
+    // create Engineer class object
+    return new Engineer(data.name, data.id, data.email, data.github);
+    // return object created
 }
 
-function createInternCard (data) {
-    // create card with data
-
-    // return card
+function createIntern (data) {
+    // create Intern class object
+    return new Intern(data.name, data.id, data.email, data.school);
+    // return object created
 }
 
 async function init() {
     const team = [];
-    team.push(createManagerCard(await inquirer.prompt(managerQuestions)));
+    team.push(createManager(await inquirer.prompt(managerQuestions)));
     var choice = (await inquirer.prompt(repeatQuestion)).choice;
     while (choice != 'finish') {
         if (choice == 'engineer') {
-            team.push(createEngineerCard(await inquirer.prompt(engineerQuestions)));
+            team.push(createEngineer(await inquirer.prompt(engineerQuestions)));
         }
         if (choice == 'intern') {
-            team.push(createInternCard(await inquirer.prompt(internQuestions)));
+            team.push(createIntern(await inquirer.prompt(internQuestions)));
         } 
         choice = (await inquirer.prompt(repeatQuestion)).choice;
     }
